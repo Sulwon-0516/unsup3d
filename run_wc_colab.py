c73fa1a5663a83213c9ec96ae917614422c23c8b
Sulwon-0516 <ininin0516@kaist.ac.kr>
2022-07-21 02:53:25 +0900

    add colab

--- run_wc_colab.py
+++ run_wc_colab.py
@@ -5,7 +5,8 @@ import torch
 from unsup3d import setup_runtime, Trainer_, Unsup3D
 import os
 
-IS_COLAB = False
+IS_COLAB = True
+ROOT_DIR = '/content/'
 
 
 
@@ -15,13 +16,13 @@ parser.add_argument('--config', default='experiments/train_celeba_64_b16.yml', t
 parser.add_argument('--gpu', default=0, type=int, help='Specify a GPU device')
 parser.add_argument('--num_workers', default=4, type=int, help='Specify the number of worker threads for data loaders')
 parser.add_argument('--seed', default=0, type=int, help='Specify a random seed')
-parser.add_argument('--save_dir', default='/home/unsup3d/result/celeba_v3', type = str, help = 'directory, that result will be saved')
+parser.add_argument('--save_dir', default='/content/drive/MyDrive/Unsup3D', type = str, help = 'directory, that result will be saved')
 parser.add_argument('--exp_name', default='debug_wc', type = str, help = 'name of folder, will be generated in save_dir')
 args = parser.parse_args()
 
 ## set up
 cfgs = setup_runtime(args)
-trainer = Trainer_(cfgs, Unsup3D, IS_COLAB, None)
+trainer = Trainer_(cfgs, Unsup3D, IS_COLAB, ROOT_DIR)
 
 run_train = cfgs.get('run_train', False)
 run_test = cfgs.get('run_test', False)
